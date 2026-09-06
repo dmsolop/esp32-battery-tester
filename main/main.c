ad0c0e1a920649921225a4f737761c4c4e379c6b
Dmytro Solop <dmtsolop@gmail.com>
2026-09-06 21:16:17 +0300

    Initial commit: project architecture and Kconfig

---
+++ main/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+enum main_states
+{
+    STATE_INIT,
+    STATE_IDLE,
+    STATE_PRE_CHECK,
+    STATE_REL_CALIBRATION,
+    STATE_DISCHARGING,
+    STATE_FINISHED
+};
+
+void app_main(void)
+{
+}
