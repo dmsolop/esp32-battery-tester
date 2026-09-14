e89248b651126df3a745cfda0d1fd6bde6bd78d4
Dmytro Solop <dmtsolop@gmail.com>
2026-09-15 00:02:35 +0300

    Fix CLI

--- components/cli/cli.c
+++ components/cli/cli.c
@@ -10,65 +10,79 @@ static const char *TAG = "CLI";
 static void cli_task(void *arg)
 {
     char line[128];
+    int pos = 0;
+
     ESP_LOGI(TAG, "CLI started. Type: start <ch>, stop <ch>, fault <ch>, set_v <ch> <uV>");
 
     while (1)
     {
-        // Читаємо рядок зі стандартного вводу (UART)
-        if (fgets(line, sizeof(line), stdin) != NULL)
+        int c = fgetc(stdin);
+
+        if (c == EOF)
         {
-            // Видаляємо символи переносу рядка
-            line[strcspn(line, "\r\n")] = 0;
-            if (strlen(line) == 0)
-                continue;
+            vTaskDelay(pdMS_TO_TICKS(50));
+            continue;
+        }
 
-            char cmd[16];
-            int ch = -1;
-            uint32_t val = 0;
+        // Відлуння: відправляємо символ назад у термінал
+        fputc(c, stdout);
+        fflush(stdout);
 
-            // Парсимо рядок: очікуємо рядок, ціле число, і (опціонально) ще одне число
-            int parsed = sscanf(line, "%15s %d %lu", cmd, &ch, &val);
+        if (c == '\n' || c == '\r')
+        {
+            // Щоб після Enter лог не злипався з нашим вводом
+            fputc('\n', stdout);
 
-            if (parsed >= 2 && ch >= 0 && ch < 4)
+            if (pos > 0)
             {
-                if (strcmp(cmd, "start") == 0)
+                line[pos] = '\0';
+
+                char cmd[16];
+                int ch = -1;
+                uint32_t val = 0;
+
+                int parsed = sscanf(line, "%15s %d %lu", cmd, &ch, &val);
+
+                if (parsed >= 2 && ch >= 0 && ch < 4)
                 {
-                    ESP_LOGI(TAG, "=> Command received: START channel %d", ch);
-                    // TODO: system_state_send_command(ch, CMD_START);
-                }
-                else if (strcmp(cmd, "stop") == 0)
-                {
-                    ESP_LOGI(TAG, "=> Command received: STOP channel %d", ch);
-                    // TODO: system_state_send_command(ch, CMD_STOP);
-                }
-                else if (strcmp(cmd, "fault") == 0)
-                {
-                    ESP_LOGI(TAG, "=> Command received: FAULT channel %d", ch);
-                    // TODO: trigger_safety_fault(ch);
-                }
-                else if (strcmp(cmd, "set_v") == 0 && parsed == 3)
-                {
-                    ESP_LOGI(TAG, "=> Command received: SET_V channel %d to %lu uV", ch, val);
-                    // TODO: adc_driver_set_mock_voltage(ch, val);
+                    if (strcmp(cmd, "start") == 0)
+                    {
+                        ESP_LOGI(TAG, "=> Command received: START channel %d", ch);
+                    }
+                    else if (strcmp(cmd, "stop") == 0)
+                    {
+                        ESP_LOGI(TAG, "=> Command received: STOP channel %d", ch);
+                    }
+                    else if (strcmp(cmd, "fault") == 0)
+                    {
+                        ESP_LOGI(TAG, "=> Command received: FAULT channel %d", ch);
+                    }
+                    else if (strcmp(cmd, "set_v") == 0 && parsed == 3)
+                    {
+                        ESP_LOGI(TAG, "=> Command received: SET_V channel %d to %lu uV", ch, val);
+                    }
+                    else
+                    {
+                        ESP_LOGW(TAG, "Unknown command: %s", cmd);
+                    }
                 }
                 else
                 {
-                    ESP_LOGW(TAG, "Unknown command: %s", cmd);
+                    ESP_LOGW(TAG, "Invalid format. Use: <cmd> <ch> [val]");
                 }
-            }
-            else
-            {
-                ESP_LOGW(TAG, "Invalid format. Use: <cmd> <ch> [val]");
+
+                pos = 0;
             }
         }
-        // Невелика затримка, щоб не блокувати процесор (хоча fgets блокує сам)
-        vTaskDelay(pdMS_TO_TICKS(10));
+        else if (c >= 32 && c <= 126 && pos < sizeof(line) - 1)
+        {
+            line[pos++] = (char)c;
+        }
     }
 }
 
 esp_err_t cli_init(void)
 {
-    // Створюємо таску з низьким пріоритетом (це лише інтерфейс)
     if (xTaskCreate(cli_task, "cli_task", 4096, NULL, 2, NULL) != pdPASS)
     {
         return ESP_FAIL;
