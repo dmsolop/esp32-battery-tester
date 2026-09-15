aefff2dee2f822fdb9942ca1d53fa439072826e9
Dmytro Solop <dmtsolop@gmail.com>
2026-09-16 00:03:54 +0300

    Integrate ADC reads and emergency abort in load_control

--- components/load_control/load_control.c
+++ components/load_control/load_control.c
@@ -1,5 +1,6 @@
 #include "load_control.h"
 #include "system_state.h"
+#include "adc_driver.h"
 #include "esp_log.h"
 #include "esp_timer.h"
 
@@ -9,6 +10,21 @@
 
 static const char *TAG = "LOAD_CTRL";
 static TaskHandle_t s_pid_tasks[CONFIG_MAX_CHANNELS] = {NULL};
+
+// Тимчасова заглушка для апаратного керування MOSFET (ШІМ / ЦАП)
+static void hw_set_load_pwm(uint8_t channel, uint32_t duty)
+{
+    if (duty == 0)
+    {
+        // Гарантоване закриття транзистора (наприклад, ledc_set_duty(..., 0))
+        // ESP_LOGD(TAG, "CH%d: MOSFET OFF", channel);
+    }
+    else
+    {
+        // Встановлення робочого ШІМ
+        // ESP_LOGD(TAG, "CH%d: MOSFET PWM = %lu", channel, duty);
+    }
+}
 
 // Функція таски для окремого каналу
 static void pid_control_task(void *pvParameters)
@@ -21,18 +37,20 @@ static void pid_control_task(void *pvParameters)
     while (1)
     {
         // Очікуємо повідомлення (Direct Task Notification) з таймаутом 50 мс.
-        // Якщо прилетить сигнал від safety_monitor (значення > 0), негайно зупиняємось.
+        // Це визначає частоту циклу ПІД-регулятора (20 Гц).
         uint32_t notification = ulTaskNotifyTake(pdTRUE, pdMS_TO_TICKS(50));
 
         system_state_get_metrics(channel, &metrics);
 
+        // Реакція на сигнал аварії від Task_Safety
         if (notification > 0)
         {
-            ESP_LOGW(TAG, "CH%d: Emergency Abort received!", channel);
-            // TODO: Апаратне відключення ШІМ (закрити MOSFET IRLZ44N)
+            ESP_LOGW(TAG, "CH%d: Emergency Abort received! Cutting power.", channel);
+            hw_set_load_pwm(channel, 0); // МИТТЄВЕ апаратне відключення
+
             metrics.state = STATE_ERROR;
             system_state_set_metrics(channel, &metrics);
-            continue;
+            continue; // Пропускаємо решту циклу, чекаємо наступної ітерації
         }
 
         int64_t current_time_us = esp_timer_get_time();
@@ -43,18 +61,31 @@ static void pid_control_task(void *pvParameters)
         switch (metrics.state)
         {
         case STATE_IDLE:
-            // Очікування команди. ШІМ вимкнено.
+        case STATE_FINISHED:
+        case STATE_ERROR:
+            // У цих станах навантаження має бути гарантовано вимкнене
+            hw_set_load_pwm(channel, 0);
             break;
 
         case STATE_PRE_CHECK:
-            // TODO: Зчитування V_ocv, перехід у DISCHARGING або CHARGING
+            // Навантаження ще вимкнене, але ми маємо зчитати напругу розімкнутого кола (Vocv)
+            hw_set_load_pwm(channel, 0);
+            adc_driver_read_voltage(channel, &metrics.voltage_uv);
+
+            // Записуємо Vocv в систему, щоб UI або інша логіка могли прийняти рішення про старт
+            system_state_set_metrics(channel, &metrics);
             break;
 
         case STATE_DISCHARGING:
-            // 1. TODO: Зчитування АЦП ADS1115
-            // 2. TODO: Розрахунок PID і оновлення ШІМ
+            // 1. Зчитування реальних (або замоканих) даних з АЦП
+            adc_driver_read_voltage(channel, &metrics.voltage_uv);
+            adc_driver_read_current(channel, &metrics.current_ua);
 
-            // 3. Інтегрування ємності та енергії (точність до мікросекунд)
+            // 2. TODO: Розрахунок PID і оновлення ШІМ
+            // uint32_t calc_duty = pid_compute(...);
+            // hw_set_load_pwm(channel, calc_duty);
+
+            // 3. Інтегрування ємності та енергії
             // Заряд = Струм (мкА) * Час (с) -> мкА*с
             metrics.accumulated_uas += (metrics.current_ua * dt_us) / 1000000;
 
@@ -70,11 +101,6 @@ static void pid_control_task(void *pvParameters)
             system_state_set_metrics(channel, &metrics);
             break;
 
-        case STATE_ERROR:
-        case STATE_FINISHED:
-            // TODO: Гарантувати, що ШІМ вимкнено
-            break;
-
         default:
             break;
         }
@@ -88,7 +114,6 @@ esp_err_t load_control_init(void)
         char task_name[16];
         snprintf(task_name, sizeof(task_name), "pid_task_%lu", i);
 
-        // Пріоритет нижчий за safety, але вищий за UI
         BaseType_t res = xTaskCreate(pid_control_task, task_name, 4096, (void *)i, configMAX_PRIORITIES - 2, &s_pid_tasks[i]);
         if (res != pdPASS)
         {
